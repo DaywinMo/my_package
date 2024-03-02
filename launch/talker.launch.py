5adf141f6390f2189cfbcae0658daf11e9250183
DaywinMo <wanchana.preedawongsakul@gmail.com>
2024-03-02 22:04:49 +0700

    second commit

--- launch/talker.launch.py
+++ launch/talker.launch.py
@@ -2,9 +2,9 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LaunchDescription({
+    return LaunchDescription([
         Node(
-            package='demo_node_cpp',
-            excutable='talker'
+            package='demo_nodes_cpp',
+            executable='talker'
         )
-    })
+    ])
